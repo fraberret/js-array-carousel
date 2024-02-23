29818184aab16bdae7f1908636aaa6bf75f63a58
fraberret <f.berretta02@gmail.com>
2024-02-23 16:29:51 +0100

    starting to work at MILESTON 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 console.log('It works');
+
+/*Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+*/
+
+//creare un array
+let slides =['01.webp','02.webp','03.webp','04.webp','05.webp']
+console.log(slides);
+
+//variabile dove salvo la posizione dell immagine attiva
+const activeImage= 0
+
+//seleziono dalla DOM 
+let slidesElement=document.querySelector('.slides')
