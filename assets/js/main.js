c4bd3cfeb429bee2eaa9278b68c0463891cd5ab2
fraberret <f.berretta02@gmail.com>
2024-02-23 16:50:25 +0100

    finish MILESTONE 2 and starting MILESTONE 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 console.log('It works');
 
-/*Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+/*
+MILESTONE 1
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
+MILESTONE 2
+Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 */
 
 //creare un array
@@ -24,3 +29,9 @@ for (let index = 0; index < slides.length; index++) {
     slidesElement.insertAdjacentHTML('beforeend', slidesMarkup)
     
 }
+
+document.querySelector('.button_up').addEventListener('click',function(){
+
+
+
+})
